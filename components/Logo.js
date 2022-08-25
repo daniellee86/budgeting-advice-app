009448dad1afde8ae1ad8dc12f8a080a588559b8
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-08-25 13:04:46 +0100

    add Link to logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,10 +1,13 @@
+import Link from 'next/link'
 const Logo = () => {
 
     return (
     <div id='logo-wrapper'>
-            <a rel="noreferrer" href="/" tabIndex="20">
-              <img className='mx-auto w-auto h-12 md:h-20 lg:h-16' src='nav-logo.svg' alt='Monzo logo'></img>
-            </a>
+      <Link href="/" tabIndex="20">
+       <a>
+        <img className='mx-auto w-auto h-12 md:h-20 lg:h-16' src='nav-logo.svg' alt='Monzo logo'></img>
+       </a>
+      </Link>
     </div>
     )
   }
