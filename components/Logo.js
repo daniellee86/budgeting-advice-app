e2df6e98cc8b8bee722422cdb15c9d1a4d6b52fd
Daniel Clough <danielleeclough86@hotmail.co.uk>
2022-11-17 17:26:33 +0000

    display error on vercel

--- components/Logo.js
+++ components/Logo.js
@@ -1,12 +1,15 @@
 const Logo = () => {
-
-    return (
-    <div id='logo-wrapper'>
-            <a rel="noreferrer" href="https://monzo.com/" tabIndex="20">
-              <img className='mx-auto w-auto h-12 md:h-20 lg:h-16' src='nav-logo.svg' alt='Monzo logo'></img>
-            </a>
+  return (
+    <div id="logo-wrapper">
+      <a rel="noreferrer" href="https://monzo.com/" tabIndex="20">
+        <img
+          className="mx-auto w-auto h-12 md:h-20 lg:h-16"
+          src="nav-logo.svg"
+          alt="Monzo logo"
+        ></img>
+      </a>
     </div>
-    )
-  }
-  
-  export default Logo
+  );
+};
+
+export default Logo;
